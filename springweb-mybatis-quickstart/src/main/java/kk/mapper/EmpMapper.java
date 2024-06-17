3bed7b1a7595d024a3ea6d78fbf37f8a32954da6
kk <1254440527@qq.com>
2024-06-17 21:59:25 +0800

    126 6/17 增加

--- springweb-mybatis-quickstart/src/main/java/kk/mapper/EmpMapper.java
+++ springweb-mybatis-quickstart/src/main/java/kk/mapper/EmpMapper.java
@@ -1,7 +1,22 @@
 package kk.mapper;
 
+import kk.pojo.Emp;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface EmpMapper {
+
+    // 根据id删除数据
+    @Delete("delete from emp where id = #{id}")
+//    public void delete (Integer id);  // 无返回值
+    public int delete (Integer id);  // 默认返回删除数
+
+    // 新增员工
+    @Options(useGeneratedKeys = true,keyProperty = "id")  // 把产生id封装进emp
+    @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
+            "values (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, #{deptId}, #{createTime}, #{updateTime})")
+    public void insert(Emp emp);
 }
--- springweb-mybatis-quickstart/src/test/java/kk/SpringwebMybatisQuickstartApplicationTests.java
+++ springweb-mybatis-quickstart/src/test/java/kk/SpringwebMybatisQuickstartApplicationTests.java
@@ -1,17 +1,24 @@
 package kk;
 
+import kk.mapper.EmpMapper;
 import kk.mapper.UserMapper;
+import kk.pojo.Emp;
 import kk.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @SpringBootTest  //代表该测试类已经与SpringBoot整合。
 class SpringwebMybatisQuickstartApplicationTests {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private EmpMapper empMapper;
 
     @Test
     public void testListUser() {
@@ -22,8 +29,28 @@ class SpringwebMybatisQuickstartApplicationTests {
         });
         //
         userList.forEach(System.out::println);
-
+    }
+    @Test
+    public void testDelte(){
+        int count = empMapper.delete(16);
+        System.out.println(count);
+    }
+    @Test
+    public void testInsert(){
+        Emp emp = new Emp();
+        emp.setUsername("tom2");
+        emp.setName("汤姆2");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)1);
+        emp.setEntrydate(LocalDate.of(2000,1,1));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+        empMapper.insert(emp);
+        System.out.println("id = "+ emp.getId());
 
     }
+
 
 }
