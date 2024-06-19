61d849f930a768f7b78cbbb7b81a1734679439f2
kk <1254440527@qq.com>
2024-06-19 10:38:47 +0800

    134 6/18 mybatis

--- springweb-mybatis-quickstart/src/main/java/kk/mapper/EmpMapper.java
+++ springweb-mybatis-quickstart/src/main/java/kk/mapper/EmpMapper.java
@@ -1,10 +1,10 @@
 package kk.mapper;
 
 import kk.pojo.Emp;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface EmpMapper {
@@ -19,4 +19,45 @@ public interface EmpMapper {
     @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
             "values (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, #{deptId}, #{createTime}, #{updateTime})")
     public void insert(Emp emp);
+    /**
+     * 根据id修改员工信息
+     */
+    @Update("update emp set username=#{username}, name=#{name}, gender=#{gender}, image=#{image}, job=#{job}, " +
+            "entrydate=#{entrydate}, dept_id=#{deptId}, update_time=#{updateTime} where id=#{id}")
+    public void update(Emp emp);
+    // 动态更新员工
+    public void update2(Emp emp);
+
+
+
+    // 查询员工
+    //
+    @Select("select id, username, password, name, gender, image, job, entrydate, dept_id, create_time, update_time " +
+            "from emp where id=#{id}")
+    public Emp getById(Integer id);
+
+    // 当未配置mybatis驼峰命名自动映射开关 tb_emp  -> tbEmp 无法映射
+//    手动结果映射：通过 @Results及@Result 进行手动结果映射
+//    起别名：在SQL语句中，对不一样的列名起别名，别名和实体类属性名一样
+//    @Select("select id, username, password, name, gender, image, job, entrydate, " +
+//            "dept_id AS deptId, create_time AS createTime, update_time AS updateTime " +
+//            "from emp " +
+//            "where id=#{id}")
+//    public Emp getById(Integer id);
+
+    // 条件查询 （解决SQL注入风险）
+    //使用MySQL提供的字符串拼接函数：concat('%' , '关键字' , '%')
+//    @Select("select * from emp " +
+//            "where name like concat('%',#{name},'%') " +
+//            "and gender = #{gender} " +
+//            "and entrydate between #{begin} and #{end} " +
+//            "order by update_time desc")
+//    public List<Emp> slectList(String name, Short gender, LocalDate begin, LocalDate end);
+
+    // sql xml文件
+    public List<Emp> slectList(String name, Short gender, LocalDate begin, LocalDate end);
+
+//批量删除
+    public void deleteByIds(List<Integer> ids);
+
 }
--- springweb-mybatis-quickstart/src/test/java/kk/SpringwebMybatisQuickstartApplicationTests.java
+++ springweb-mybatis-quickstart/src/test/java/kk/SpringwebMybatisQuickstartApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -30,26 +31,72 @@ class SpringwebMybatisQuickstartApplicationTests {
         //
         userList.forEach(System.out::println);
     }
+
     @Test
-    public void testDelte(){
+    public void testDelte() {
         int count = empMapper.delete(16);
         System.out.println(count);
     }
+
     @Test
-    public void testInsert(){
+    public void testDeleteByIds() {
+        List<Integer> ids = Arrays.asList(11, 12, 13);
+        empMapper.deleteByIds(ids);
+    }
+
+    @Test
+    public void testInsert() {
         Emp emp = new Emp();
         emp.setUsername("tom2");
         emp.setName("汤姆2");
         emp.setImage("1.jpg");
-        emp.setGender((short)1);
-        emp.setJob((short)1);
-        emp.setEntrydate(LocalDate.of(2000,1,1));
+        emp.setGender((short) 1);
+        emp.setJob((short) 1);
+        emp.setEntrydate(LocalDate.of(2000, 1, 1));
         emp.setCreateTime(LocalDateTime.now());
         emp.setUpdateTime(LocalDateTime.now());
         emp.setDeptId(1);
         empMapper.insert(emp);
-        System.out.println("id = "+ emp.getId());
+        System.out.println("id = " + emp.getId());
 
+    }
+
+    //修改的员工信息
+    @Test
+    public void testUpdate() {
+        //要修改的员工信息
+        Emp emp = new Emp();
+        emp.setId(3);
+        emp.setUsername("song");
+        emp.setName("老宋");
+        emp.setGender((short) 2);
+//        emp.setImage("2.jpg");
+//        emp.setPassword(null);
+//        emp.setJob((short)2);
+//        emp.setEntrydate(LocalDate.of(2012,1,1));
+//        emp.setCreateTime(null);
+//        emp.setDeptId(2);
+        emp.setUpdateTime(LocalDateTime.now());
+        //调用方法，修改员工数据
+//        empMapper.update(emp);
+        empMapper.update2(emp);
+    }
+
+    // select 查询员工
+    @Test
+    public void testGetById() {
+        Emp emp = empMapper.getById(1);
+        System.out.println(emp);
+    }
+
+    // 动态select 查询员工
+    @Test
+    public void testList() {
+//        List<Emp> list = empMapper.slectList("张", (short) 1, LocalDate.of(2010, 1, 1), LocalDate.of(2020, 12, 12));
+        //动态sql
+//        List<Emp> list = empMapper.slectList("张", null, null, null);
+        List<Emp> list = empMapper.slectList(null, (short) 1, null, null);
+        System.out.println(list);
     }
 
 
